fcff5913ae48b0a7bf6bca14736819ec6f4ebe77
Vishal Jayapalan <vishal935@gmail.com>
2022-09-18 22:22:35 +0530

    changed the backend port to 4000

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').createServer(app)
 const cors = require('cors')
 require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 const path = require('path')
 
 const io = require('socket.io')(http)
